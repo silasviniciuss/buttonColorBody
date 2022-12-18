6e0a4ed518fb284dd951a2b5c4f64b9bd06ba598
silasviniciuss <silasvinicius.dev@gmail.com>
2022-12-18 01:16:55 -0300

    corrigindo o texto do title e alterando o style do button para mudar a cor

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,15 @@ var dark_theme = false;
 function change_background(){
     if (dark_theme == false){
         dark_theme = true;
-        document.body.style.background = "black";
+        document.body.style.background = "black"; 
+        button.style.background = "white";
+        button.style.color = "black";       
     }
     else{
         dark_theme = false;
         document.body.style.background = "white";
+        button.style.background = "black";
+        button.style.color = "White";
     }
 
 }
